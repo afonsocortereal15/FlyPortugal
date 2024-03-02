3142ddc00b199048b39045c41f3431050a4f8cb0
Afonso Corte-Real <afonsocortereal15@gmail.com>
2024-03-02 17:36:52 +0000

    Update search/home page

--- index.php
+++ index.php
@@ -10,12 +10,11 @@
   <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
 
   <!-- Bootstrap -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 
   <!-- Bootstrap Icons-->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
   <!-- Core theme CSS -->
   <link rel="stylesheet" href="css/style.css">
@@ -24,14 +23,11 @@
 
 <body class="d-flex flex-column h-100">
   <main class="flex-shrink-0 min-vh-100">
-    <!-- Navigation -->
+    <!-- Navbar -->
     <nav class="navbar navbar-expand-lg" style="padding-top: 20px">
       <div class="container px-5">
-        <a class="navbar-brand" href="../" style="width: 200px"><img src="assets/flyportugal-logo.png"
-            width="100%" /></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" \
-          data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-          aria-label="Toggle navigation">
+        <a class="navbar-brand" href="./" style="width: 200px"><img src="assets/flyportugal-logo.png" width="100%" /></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" \ data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="bi bi-list"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -50,8 +46,8 @@
         </div>
       </div>
     </nav>
-    <!-- Header -->
-    <header class=" py-5">
+    <!-- Main -->
+    <section class=" py-5">
       <div class="container px-5">
         <div class="row gx-5 align-items-center justify-content-center">
           <div class="col-lg-8 col-xl-7 col-xxl-6">
@@ -65,14 +61,16 @@
               </p>
               <div class="search">
                 <i class="bi bi-search"></i>
-                <input type="text" class="form-control col-sm-12 col-md-6" placeholder="Flight Number" />
-                <button class="btn btn-primary">Search</button>
+                <form action="flight/index.php" method="get">
+                  <input type="text" class="form-control col-sm-12 col-md-6" placeholder="Flight Number" name="flight" value="<?php echo $_COOKIE['flight']; ?>" />
+                  <button class="btn btn-primary" type="submit">Search</button>
+                </form>
               </div>
             </div>
           </div>
-        </div>  
+        </div>
       </div>
-    </header>
+    </section>
   </main>
   <footer>
     <div class="container px-5 align-content-center small m-0 text-dark">
