fd1e9bf864e4fe7778dd26f40273b6f9a719e112
Afonso Corte-Real <afonsocortereal15@gmail.com>
2024-07-10 18:37:26 +0100

    Update Navbar

--- public/main.php
+++ public/main.php
@@ -29,23 +29,6 @@ include("../src/flightSearch.php");
     <nav class="navbar navbar-expand-lg" style="padding-top: 20px">
       <div class="container px-5">
         <a class="navbar-brand" href="../" style="width: 200px"><img src="../assets/img/flyportugal-logo.png" width="100%" /></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" \ data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="bi bi-list"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link" href="../">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="../login">Login</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About Us</a>
-            </li>
-            <li></li>
-          </ul>
-        </div>
       </div>
     </nav>
     <!-- Flight Info -->
