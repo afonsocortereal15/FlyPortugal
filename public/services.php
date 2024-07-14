ad52be39ab9659efe298cbdac9a10e276558e700
Afonso Corte-Real <afonsocortereal15@gmail.com>
2024-07-14 23:48:24 +0100

    Change background

--- public/services.php
+++ public/services.php
@@ -16,7 +16,7 @@ include("../src/getServices.php");
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  
+
   <!-- Bootstrap Icons-->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
 
@@ -25,8 +25,8 @@ include("../src/getServices.php");
   <link rel="stylesheet" type="text/css" href="../assets/css/services.css">
 </head>
 
-<body class="d-flex flex-column h-100">
-  <main class="flex-shrink-0 min-vh-100" style="background-image: url('<?php echo $bg_image; ?>')">
+<body class="d-flex flex-column h-100" style="background: DarkSlateGray">
+  <main class="flex-shrink-0 min-vh-100">
     <!-- Navabar -->
     <nav class="navbar navbar-expand-lg" style="padding-top: 20px">
       <div class="container px-5">
@@ -36,12 +36,12 @@ include("../src/getServices.php");
     <section class="py-3">
       <div class="container px-5">
         <div class="row gx-5 align-items-center justify-content-center">
-          <div class="col-md-11 col-sm-10 col-10 bg-white rounded-3" style="margin-bottom: 100px;">
+          <div class="col-md-11 col-sm-10 col-10 bg-white rounded-3 shadow" style="margin-bottom: 100px;">
             <!-- Flight Details -->
             <div class="flight-card text-right text-xl-start">
               <div class="row">
                 <div class="col align-self-center text-center">
-                  <img class="airline-logo" src="<?php echo $airline_logo ?>" alt="<?php echo $airline_name; ?> Logo" title="<?php echo $airline_name; ?> Logo" width="65%">
+                  <img class="airline-logo" src="<?php echo $airline_logo ?>" alt="<?php echo $airline_name; ?> Logo" title="<?php echo $airline_name; ?> Logo" width="55%">
                 </div>
                 <div class="col align-self-center text-center">
                   <h1 class="airline-name fw-bolder text-dark" title="Airline">
@@ -87,6 +87,13 @@ include("../src/getServices.php");
                       <?php echo $status ?>
                     </span>
                   </h4>
+                  <?php if ($status == "Scheduled") : ?>
+                    <h4>
+                      <span id="depCountdown" class="badge" title="Time Until Departure">
+                        00:00
+                      </span>
+                    </h4>
+                  <?php endif; ?>
                 </div>
                 <div class="col-12 col-md-4 text-center align-self-center" style="padding: 12px;">
                   <p class="lead fw-normal text-dark text-center" title="Scheduled Arrival Time">
@@ -127,17 +134,17 @@ include("../src/getServices.php");
 
               <!-- Coffeeshops tab -->
               <div class="tab-pane fade" id="pills-coffeeshops" role="tabpanel" aria-labelledby="pills-coffeeshops-tab">
-                <?php printServices($dep_iata, "coffeeshops") ?>
+                <?php printServices($dep_iata, "1") ?>
               </div>
 
               <!-- Restaurants tab -->
               <div class="tab-pane fade" id="pills-restaurants" role="tabpanel" aria-labelledby="pills-restaurants-tab">
-                <?php printServices($dep_iata, "restaurants") ?>
+                <?php printServices($dep_iata, "2") ?>
               </div>
 
               <!-- Stores tab -->
               <div class="tab-pane fade" id="pills-stores" role="tabpanel" aria-labelledby="pills-stores-tab">
-                <?php printServices($dep_iata, "stores") ?>
+                <?php printServices($dep_iata, "3") ?>
               </div>
             </div>
           </div>
@@ -146,9 +153,57 @@ include("../src/getServices.php");
     </section>
   </main>
   <footer>
-    <div class="container px-5 align-content-center small m-0 text-dark">
+    <div class="container px-5 align-content-center small m-0 text-white">
       Copyright &copy; FlyPortugal 2024 - All rights reserved.
     </div>
   </footer>
 </body>
+
 </html>
+
+<script>
+  // Check the status of a flight and perform the corresponding action
+  if ("<?php echo $status ?>" == "En-Route") {
+    // If the flight is en-route, update the flight time every second
+    arrCountdown();
+    setInterval(arrCountdown, 1000);
+  } else if ("<?php echo $status ?>" == "Scheduled") {
+    // If the flight is scheduled, update the departure countdown every second
+    depCountdown();
+    setInterval(depCountdown, 1000);
+  }
+
+  function depCountdown() {
+    // Set the departure time based on the estimated or actual departure time
+    if ("<?php echo $dep_estimated; ?>" === "n/a") {
+      dep_time = <?php echo $dep_time_ts; ?>;
+    } else {
+      dep_time = <?php echo $dep_estimated_ts; ?>;
+    }
+
+    // Calculate the current time in seconds
+    const time = Date.now() / 1000;
+
+    // Calculate the remaining seconds until departure
+    seconds_left = (dep_time - time) + 60;
+
+    // Format the remaining time as a string (HH:MM)
+    time_left = new Date(seconds_left * 1000).toISOString().substring(11, 16);
+
+    // Update the badge background color based on the remaining time
+    if (seconds_left < 3600) {
+      document.getElementById("depCountdown").classList.add('text-bg-warning');
+    } else if (seconds_left < 1800) {
+      document.getElementById("depCountdown").classList.add('text-bg-danger');
+    } else {
+      document.getElementById("depCountdown").classList.add('text-bg-success');
+    }
+
+    // Display the remaining time or "n/a" if the departure time has passed
+    if (seconds_left < 0) {
+      document.getElementById("depCountdown").innerHTML = "n/a";
+    } else {
+      document.getElementById("depCountdown").innerHTML = time_left;
+    }
+  }
+</script>
